02cc9d9dc76c40c8b148b031d847a7079ae4dce5
Andrew Felsher <ncsuandrew12@gmail.com>
2018-07-30 10:30:39 -0400

    Update copyright/license text.

--- Imperio/projects/examples/Rush/src/com/imperio/examples/rush/ErrorHandler.java
+++ Imperio/projects/examples/Rush/src/com/imperio/examples/rush/ErrorHandler.java
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -44,7 +44,7 @@ public class ErrorHandler implements com.imperio.ErrorHandler {
         System.err.println(
                 "###############################################################################");
         if (err != ErrorType.UNKNOWN_OPTION) {
-            System.err.print(impApp);
+            System.err.print(impApp.getUsageString());
             System.err.println();
             System.err.printf(err.msg);
             System.err.println();
